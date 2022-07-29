89c3f0eaab1a84fc7819c91404b876bf5fece9a9
noshi <jamejame0921426546@gmail.com>
2022-07-29 13:54:35 +0800

    關於未設頭像而引發的錯誤修正

--- cod/mes.py
+++ cod/mes.py
@@ -9,7 +9,7 @@ class mes (Cog_Extension):
   async def 深淵(self,interaction:nextcord.Interaction,name:str,uid:int=nextcord.SlashOption(name='uid'),help=nextcord.SlashOption(name='需要幫忙的項目')):
     emb=nextcord.Embed(title="溫迪深淵代打", description="★ 提供代打深境螺旋或是需要角色健檢及隊伍建議的 ★", color=0xffd6d6)
     emb.set_author(name="温迪#7304", url="https://discord.com/channels/@me/988654327990214656", icon_url="https://cdn.discordapp.com/avatars/537846086749126657/c3ae37775b12e038a1d0c57528ea9e6a.webp?size=80")
-    emb.set_thumbnail(url=interaction.user.avatar.url)
+    emb.set_thumbnail(url=interaction.user.display_avatar.url)
     emb.add_field(name="資訊", value=f"遊戲名稱:{name}\n UID:{uid}\n 需要幫忙的項目:{help}\n", inline=False)
     emb.add_field(name="已完成代打申請", value="請先將遊戲帳號密碼私訊温迪，若方便也可附上你的角色圖鑑+練度**[温迪#7304](https://discord.com/channels/@me/988654327990214656)**", inline=False)
     emb.add_field(name="★重要★ 代打結束後，請你登入遊戲後到深境螺自行領取獎勵。", value="​", inline=False)
