b76f029dd43e5ebca63b8e9896661897a6cd16d5
noshi <jamejame0921426546@gmail.com>
2022-08-06 21:19:51 +0800

    modified:   cod/ban_list.py

--- cod/ban_list.py
+++ cod/ban_list.py
@@ -3,13 +3,14 @@ from core.classes import Cog_Extension
 import nextcord
 import json
 from nextcord.ext import commands
-with open('test.json','r',encoding='utf-8') as ban_l:
-  banlast=json.load(ban_l)
+
 print(banlast)
 class ban_list (Cog_Extension):
   @commands.command()
   @commands.has_any_role(978680963099942912,"工程師")
   async def ban_list(self,ctx):
+    with open('test.json','r',encoding='utf-8') as ban_l:
+      banlast=json.load(ban_l)
     em=nextcord.Embed(title="ban人名單", color=0xffe380)
     for a in banlast['ban']:
       u_name=self.bot.get_user(a)
