2c03f3f6a93acfc76e2f6c83e6a9fa70a0f72782
noshi <jamejame0921426546@gmail.com>
2022-08-08 17:47:43 +0800

    modified:   cod/ban_list.py

--- cod/ban_list.py
+++ cod/ban_list.py
@@ -4,6 +4,8 @@ import nextcord
 import json
 from nextcord.ext import commands
 
+from main import load
+
 
 class ban_list (Cog_Extension):
   @commands.command()
@@ -16,6 +18,24 @@ class ban_list (Cog_Extension):
       u_name=self.bot.get_user(a)
       em.add_field(name=f"{u_name}", value=f"{a}", inline=False)
     await ctx.send(embed=em)
+  @commands.Cog.listener()
+  async def on_member_remove(self,mem:nextcord.Member):
+    with open('test.json','r',encoding='utf-8') as ban_l:
+      banlast=json.load(ban_l)
+    if (str(mem.id)) not in (banlast['leavl']):
+      banlast['leavl'][mem.id]=1
+      with open('test.json','w',encoding='utf-8') as ban_l:
+        json.dump(banlast,ban_l)
+    elif banlast['leavl'][str(mem.id)] >=1:
+      banlast['ban'].append(int(mem.id))
+      del banlast['leavl'][str(mem.id)]
+      with open('test.json','w',encoding='utf8') as loli:
+        json.dump(banlast,loli)
+    else:
+      banlast['leavl'][str(mem.id)]+=1
+      with open('test.json','w',encoding='utf-8') as ban_l:
+        json.dump(banlast,ban_l)      
+    
 
 
 
